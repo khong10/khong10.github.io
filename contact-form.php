3e7ee6551485c616b11a732d1b4dd04640aaf0f9
Kahee Hong <khong10@myseneca.ca>
2019-06-19 12:06:16 -0400

    contact_update1

--- contact-form.php
+++ contact-form.php
@@ -3,7 +3,7 @@ $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
-$email_from = 'https://khong10.github.io/';
+$email_from = 'hksharpca@gmail.com';
 
 $email_subject = "New Form Submission";
 
