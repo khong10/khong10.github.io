ce59834ebf4036ca71163185bf63a8d3007b340c
Kahee Hong <khong10@myseneca.ca>
2019-06-19 12:25:18 -0400

    contact_update3

--- contact-form.php
+++ contact-form.php
@@ -1,20 +1,18 @@
 <?php
 $name = $_POST['name'];
-$visitor_email = $_POST['email'];
+$mailFrom = $_POST['email'];
 $message = $_POST['message'];
 
 $email_from = 'hksharpca@gmail.com';
 
 $email_subject = "New Form Submission";
 
+$to = "kaheehong10@gmail.com";
+$headers = "From: " .$email_from;
 $email_body = "User Name: $name.\n".
             "User Email: $visitor_email.\n".
             "User Message: $message.\n";
 
-$to = "kaheehong10@gmail.com";
-$headers = "From: $email_from \r\n";
-
-$headers .= "Reply-To: $visitor_email \r\n";
+$headers = "Reply-To: $mailFrom \r\n";
 mail($to, $email_subject, $email_body, $headers);
 header ("Location: main.html");
-?>
